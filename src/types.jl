937197e6a025e1327f1e933ddaf40299e2dadbcb
Peter Ahrens <ptrahrens@gmail.com>
2022-01-10 16:19:22 -0500

    more simplification

--- src/types.jl
+++ src/types.jl
@@ -5,251 +5,9 @@
 abstract type Symbolic{T} end
 
 
-# TODO_TERMINTERFACE
-
-TermInterface.symtype(x::Number) = typeof(x)
-TermInterface.symtype(::Symbolic{T}) where {T} = T
-
-TermInterface.metadata(s::Symbolic) = s.metadata
-
-TermInterface.metadata(s::Symbolic, meta) = Setfield.@set! s.metadata = meta
-
-function hasmetadata(s::Symbolic, ctx)
-    metadata(s) isa AbstractDict && haskey(metadata(s), ctx)
-end
-
-function getmetadata(s::Symbolic, ctx)
-    md = metadata(s)
-    if md isa AbstractDict
-        md[ctx]
-    else
-        throw(ArgumentError("$s does not have metadata for $ctx"))
-    end
-end
-
-function getmetadata(s::Symbolic, ctx, default)
-    md = metadata(s)
-    md isa AbstractDict ? get(md, ctx, default) : default
-end
-
-# pirated for Setfield purposes:
-Base.ImmutableDict(d::ImmutableDict{K,V}, x, y)  where {K, V} = ImmutableDict{K,V}(d, x, y)
-
-assocmeta(d::Dict, ctx, val) = (d=copy(d); d[ctx] = val; d)
-function assocmeta(d::Base.ImmutableDict, ctx, val)::ImmutableDict{DataType,Any}
-    # optimizations
-    # If using upto 3 contexts, things stay compact
-    if isdefined(d, :parent)
-        d.key === ctx && return @set d.value = val
-        d1 = d.parent
-        if isdefined(d1, :parent)
-            d1.key === ctx && return @set d.parent.value = val
-            d2 = d1.parent
-            if isdefined(d2, :parent)
-                d2.key === ctx && return @set d.parent.parent.value = val
-            end
-        end
-    end
-    Base.ImmutableDict{DataType, Any}(d, ctx, val)
-end
-
-function setmetadata(s::Symbolic, ctx::DataType, val)
-    if s.metadata isa AbstractDict
-        @set s.metadata = assocmeta(s.metadata, ctx, val)
-    else
-        # fresh Dict
-        @set s.metadata = Base.ImmutableDict{DataType, Any}(ctx, val)
-    end
-end
-
 Base.isequal(::Symbolic, x) = false
 Base.isequal(x, ::Symbolic) = false
 Base.isequal(::Symbolic, ::Symbolic) = false
-
-
-### End of interface
-
-function to_symbolic(x)
-    Base.depwarn("`to_symbolic(x)` is deprecated, define the interface for your " *
-                 "symbolic structure using `istree(x)`, `operation(x)`, `arguments(x)` " *
-                 "and `similarterm(::YourType, f, args, symtype)`", :to_symbolic, force=true)
-
-    x
-end
-
-Base.one( s::Symbolic) = one( symtype(s))
-
-Base.zero(s::Symbolic) = zero(symtype(s))
-
-"""
-    promote_symtype(f, Ts...)
-
-The result of applying `f` to arguments of [`symtype`](#symtype) `Ts...`
-
-```julia
-julia> promote_symtype(+, Real, Real)
-Real
-
-julia> promote_symtype(+, Complex, Real)
-Number
-
-julia> @syms f(x)::Complex
-(f(::Number)::Complex,)
-
-julia> promote_symtype(f, Number)
-Complex
-```
-
-When constructing [`Term`](#Term)s without an explicit symtype,
-`promote_symtype` is used to figure out the symtype of the Term.
-"""
-promote_symtype(f, Ts...) = Any
-
-const NO_METADATA = nothing
-
-#--------------------
-#--------------------
-#### Syms
-#--------------------
-"""
-    Sym{T}(name::Symbol)
-
-A named variable of type `T`. Type `T` can be `FnType{X,Y}` which
-means the variable is a function with the type signature X -> Y where
-`X` is a tuple type of arguments and `Y` is any type.
-"""
-struct Sym{T, M} <: Symbolic{T}
-    name::Symbol
-    metadata::M
-end
-
-TermInterface.issym(s::Sym) = true
-Base.nameof(s::Sym) = s.name
-
-ConstructionBase.constructorof(s::Type{<:Sym{T}}) where {T} = (n,m) -> Sym{T}(n, metadata=m)
-
-function (::Type{Sym{T}})(name; metadata=NO_METADATA) where {T}
-    Sym{T, typeof(metadata)}(name, metadata)
-end
-
-Base.hash(s::Sym{T}, u::UInt) where {T} = hash(T, hash(s.name, u))
-
-function Base.isequal(a::Sym, b::Sym)
-    symtype(a) !== symtype(b) && return false
-    isequal(nameof(a), nameof(b))
-end
-
-Base.show(io::IO, v::Sym) = Base.show_unquoted(io, v.name)
-
-#---------------------------
-#---------------------------
-#### Function-like variables
-#---------------------------
-
-# Maybe don't even need a new type, can just use Sym{FnType}
-struct FnType{X<:Tuple,Y} end
-
-(f::Symbolic{<:FnType})(args...) = Term{promote_symtype(f, symtype.(args)...)}(f, [args...])
-
-function (f::Symbolic)(args...)
-    error("Sym $f is not callable. " *
-          "Use @syms $f(var1, var2,...) to create it as a callable.")
-end
-
-"""
-    promote_symtype(f::Sym{FnType{X,Y}}, arg_symtypes...)
-
-The output symtype of applying variable `f` to arugments of symtype `arg_symtypes...`.
-if the arguments are of the wrong type then this function will error.
-"""
-function promote_symtype(f::Symbolic{FnType{X,Y}}, args...) where {X, Y}
-    if X === Tuple
-        return Y
-    end
-
-    # This is to handle `Tuple{T} where T`, so we cannot reliably query the type
-    # parameters of the `Tuple` in `FnType`.
-    t = Tuple{args...}
-    if !(t <: X)
-        error("$t is not a subtype of $X.")
-    end
-    return Y
-end
-
-"""
-    @syms <lhs_expr>[::T1] <lhs_expr>[::T2]...
-
-For instance:
-
-    @syms foo::Real bar baz(x, y::Real)::Complex
-
-Create one or more variables. `<lhs_expr>` can be just a symbol in which case
-it will be the name of the variable, or a function call in which case a function-like
-variable which has the same name as the function being called. The Sym type, or
-in the case of a function-like Sym, the output type of calling the function
-can be set using the `::T` syntax.
-
-# Examples:
-
-- `@syms foo bar::Real baz::Int` will create
-variable `foo` of symtype `Number` (the default), `bar` of symtype `Real`
-and `baz` of symtype `Int`
-- `@syms f(x) g(y::Real, x)::Int h(a::Int, f(b))` creates 1-arg `f` 2-arg `g`
-and 2 arg `h`. The second argument to `h` must be a one argument function-like
-variable. So, `h(1, g)` will fail and `h(1, f)` will work.
-"""
-macro syms(xs...)
-    defs = map(xs) do x
-        n, t = _name_type(x)
-        :($(esc(n)) = Sym{$(esc(t))}($(Expr(:quote, n))))
-        nt = _name_type(x)
-        n, t = nt.name, nt.type
-        :($(esc(n)) = Sym{$(esc(t))}($(Expr(:quote, n))))
-    end
-    Expr(:block, defs...,
-         :(tuple($(map(x->esc(_name_type(x).name), xs)...))))
-end
-
-function syms_syntax_error()
-    error("Incorrect @syms syntax. Try `@syms x::Real y::Complex g(a) f(::Real)::Real` for instance.")
-end
-
-function _name_type(x)
-    if x isa Symbol
-        return (name=x, type=Number)
-    elseif x isa Expr && x.head === :(::)
-        if length(x.args) == 1
-            return (name=nothing, type=x.args[1])
-        end
-        lhs, rhs = x.args[1:2]
-        if lhs isa Expr && lhs.head === :call
-            # e.g. f(::Real)::Unreal
-            type = map(x->_name_type(x).type, lhs.args[2:end])
-            return (name=lhs.args[1], type=:($FnType{Tuple{$(type...)}, $rhs}))
-        else
-            return (name=lhs, type=rhs)
-        end
-    elseif x isa Expr && x.head === :ref
-        ntype = _name_type(x.args[1]) # a::Number
-        N = length(x.args)-1
-        return (name=ntype.name,
-                type=:(Array{$(ntype.type), $N}),
-                array_metadata=:(Base.Slice.(($(x.args[2:end]...),))))
-    elseif x isa Expr && x.head === :call
-        return _name_type(:($x::Number))
-    else
-        syms_syntax_error()
-    end
-end
-
-function Base.show(io::IO, f::Symbolic{<:FnType{X,Y}}) where {X,Y}
-    print(io, nameof(f))
-    # Use `Base.unwrap_unionall` to handle `Tuple{T} where T`. This is not the
-    # best printing, but it's better than erroring.
-    argrepr = join(map(t->"::"*string(t), Base.unwrap_unionall(X).parameters), ", ")
-    print(io, "(", argrepr, ")")
-    print(io, "::", Y)
-end
 
 #--------------------
 #--------------------
@@ -272,27 +30,25 @@ If `T` is not provided during construction, it is queried by calling
 
 See [promote_symtype](#promote_symtype)
 """
-struct Term{T, M} <: Symbolic{T}
+struct Term
     f::Any
     arguments::Any
-    metadata::M
-    hash::Ref{UInt} # hash cache
 end
 
-function ConstructionBase.constructorof(s::Type{<:Term{T}}) where {T}
+function ConstructionBase.constructorof(s::Type{Term})
     function (f, args, meta, hash)
         Term{T, typeof(meta)}(f, args, meta, hash)
     end
 end
 
-function (::Type{Term{T}})(f, args; metadata=NO_METADATA) where {T}
-    Term{T, typeof(metadata)}(f, args, metadata, Ref{UInt}(0))
+function (::Type{Term})(f, args)
+    Term(f, args, Ref{UInt}(0))
 end
 
 TermInterface.istree(t::Type{<:Term}) = true
 
-function Term(f, args; metadata=NO_METADATA)
-    Term{_promote_symtype(f, args)}(f, args, metadata=metadata)
+function Term(f, args)
+    Term{_promote_symtype(f, args)}(f, args)
 end
 
 TermInterface.operation(x::Term) = getfield(x, :f)
@@ -315,40 +71,17 @@ end
 ## This is much faster than hash of an array of Any
 hashvec(xs, z) = foldr(hash, xs, init=z)
 
-function Base.hash(t::Term{T}, salt::UInt) where {T}
+function Base.hash(t::Term, salt::UInt)
     !iszero(salt) && return hash(hash(t, zero(UInt)), salt)
     h = t.hash[]
     !iszero(h) && return h
-    h′ = hashvec(arguments(t), hash(operation(t), hash(T, salt)))
+    h′ = hashvec(arguments(t), hash(operation(t), salt))
     t.hash[] = h′
     return h′
 end
 
-isassociative(::Any) = false
-isassociative(::Union{typeof(+),typeof(*)}) = true
-
-_promote_symtype(f::Sym, args) = promote_symtype(f, map(symtype, args)...)
-function _promote_symtype(f, args)
-    if length(args) == 0
-        promote_symtype(f)
-    elseif length(args) == 1
-        promote_symtype(f, symtype(args[1]))
-    elseif length(args) == 2
-        promote_symtype(f, symtype(args[1]), symtype(args[2]))
-    elseif isassociative(f)
-        mapfoldl(symtype, (x,y) -> promote_symtype(f, x, y), args)
-    else
-        promote_symtype(f, map(symtype, args)...)
-    end
-end
-
 function term(f, args...; type = nothing)
-    if type === nothing
-        T = _promote_symtype(f, args)
-    else
-        T = type
-    end
-    Term{T}(f, [args...])
+    Term(f, [args...])
 end
 
 """
@@ -367,11 +100,8 @@ different type than `t`, because `f` also influences the result.
 - The `symtype` of the resulting term. Best effort will be made to set the symtype of the
   resulting similar term to this type.
 """
-TermInterface.similarterm(t::Type{<:Symbolic}, f, args; metadata=nothing) = 
-    similarterm(t, f, args, _promote_symtype(f, args); metadata=metadata)
-    
-TermInterface.similarterm(t::Type{<:Term}, f, args, symtype; metadata=nothing) = 
-    Term{_promote_symtype(f, args)}(f, args; metadata=metadata)
+TermInterface.similarterm(t::Type{Term}, f, args, symtype; metadata=nothing) = 
+    Term(f, args)
 
 #--------------------
 #--------------------
@@ -594,5 +324,4 @@ function print_tree(_io::IO, x::Union{Term}; show_type=false, kw...)
     end
 end
 
-TermInterface.istree(t::Type{<:Sym}) = false
 TermInterface.istree(t::Type{<:Symbolic}) = true
