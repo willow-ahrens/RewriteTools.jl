ec1990b3c0e0a5d4ffbe6528c1528640bc8312b9
Willow Ahrens <ptrahrens@gmail.com>
2022-11-12 14:25:52 -0500

    dce

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,11 +18,6 @@ else
 end
 
 include("utils.jl")
-
-if haskey(ENV, "REWRITETOOLS_BENCHMARK_ONLY")
-    include("benchmark.jl")
-else
-    include("basics.jl")
-    include("rule.jl")
-    include("rewrite.jl")
-end
+include("basics.jl")
+include("rule.jl")
+include("rewrite.jl")
